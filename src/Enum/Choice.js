4e0116e45b8e09db08dd4ad01afadf21e646f62b
nucito <andrenucitoschmidt@gmail.com>
2025-08-08 19:32:02 +0200

    delete Js Files

--- src/Enum/Choice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Choice;
-(function (Choice) {
-    Choice["Rock"] = "rock";
-    Choice["Paper"] = "paper";
-    Choice["Scissors"] = "scissors";
-})(Choice || (Choice = {}));
-exports.default = Choice;
